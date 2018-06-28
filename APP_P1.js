2c9bfafeb3534e832d2ceca31039d5ee8dd235c4
Leilanne <36213354+leilanneq@users.noreply.github.com>
2018-06-28 16:22:27 -0300

    teste
    Arquivos copiados que eu não sei o que significa

---
+++ APP_P1.js
@@ -0,0 +1,13 @@
+// Leilanne Tavares RA 21751248
+
+var produtoParafuso = document.getElementById("parafuso");
+var valorParafuso = document.getElementById("valor-parafuso");
+var qntParafuso = document.getElementById("qnt-parafuso");
+var totalParafuso = document.getElementById ("total-parafuso");
+
+
+var precoNovo = document.querySelector("#precoNovo");
+var produtoNovo = document.querySelector("#produtoNovo");
+var qntdNova = document.querySelector("#qntNova");
+
+
